3c632b15a4a5b167b7d7bc35989e01e681ea1100
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-09-12 20:56:40 -0500

    writing data into json file

--- index.js
+++ index.js
@@ -23,11 +23,32 @@ const pathQuestions = [
 ];
 
 inquirer.prompt(pathQuestions).then((paths) => {
-  const csvData = [];
-  fs.createReadStream(paths.input)
+  const jsonData = [];
+  let columnHeaders;
+
+  fs.createReadStream(paths.input) //reading/parsing data from CSV file
     .pipe(csvParser())
     .on("data", (row) => {
-      csvData.push(row);
-      console.log(row);
+      //if no headers, first row will used as headers
+      if (!columnHeaders) {
+        columnHeaders = Object.keys(row);
+      }
+
+      const jsonObject = {}; //JSON object with header as key
+      columnHeaders.forEach((header) => {
+        jsonObject[header] = row[header];
+      });
+
+      jsonData.push(jsonObject); //pushing JSON object to results
+    })
+    .on("end", () => {
+      //Writing JSON DATA into file
+      if (paths.output) {
+        fs.writeFileSync(paths.output, JSON.stringify(jsonData, null, 2));
+        console.log(`CSV data has been written into ${paths.output} file`);
+      } else {
+        //returning JSON data if no file specified
+        console.log(JSON.stringify(jsonData, null, 2));
+      }
     });
 });
