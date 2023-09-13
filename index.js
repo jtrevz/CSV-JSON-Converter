3c0370182860680eeed3078d9d8aacc32081fae6
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-09-12 20:34:51 -0500

    prompt questions, reading csv file

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const fs = require("fs");
 const csvParser = require("csv-parser");
 const inquirer = require("inquirer");
+
+//Capturing input and output paths from clients
+const pathQuestions = [
+  {
+    name: "input",
+    type: "input",
+    message: "Enter path of CSV file you would like to convert:",
+    validate: function (path) {
+      if (fs.existsSync(path)) {
+        return true;
+      }
+      return "File was not found. Enter valid path to CSV input file.";
+    },
+  },
+  {
+    name: "output",
+    type: "input",
+    message: "Enter path to output JSON file:",
+  },
+];
+
+inquirer.prompt(pathQuestions).then((paths) => {
+  const csvData = [];
+  fs.createReadStream(paths.input)
+    .pipe(csvParser())
+    .on("data", (row) => {
+      csvData.push(row);
+      console.log(row);
+    });
+});
